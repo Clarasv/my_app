40b8f464bd6150886fb7ec91a3d158919545a810
Clarasv <mottybvb09.gmail.com>
2023-03-30 04:10:32 +0000

    likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,24 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user
+
+ def create
+   @like = Like.new(
+     user_id: @current_user.id,
+     post_id: params[:post_id]
+     )
+   if @like.save
+    redirect_to("/posts/#{params[:post_id]}", status: :unprocessable_entity)
+   end
+ end
+ 
+ def destroy
+   @like = Like.find_by(
+       user_id: @current_user.id, 
+       post_id: params[:post_id]
+       )
+   if @like.destroy
+    redirect_to("/posts/#{params[:post_id]}", status: :unprocessable_entity)
+   end 
+ end
+
+end
